44345bacd2491d2d05b721a31f948e04a9e77c0e
DiegoKoyama <yukioko.jp@gmail.com>
2022-11-17 19:57:03 -0300

    componentizacao + props dos Posts

---
+++ src/components/Posts.js
@@ -0,0 +1,8 @@
+import Post from "./Post"
+export default function Posts(){
+    return (<div class="posts">
+    <Post imagem="assets/img/meowed.svg" imagem2="assets/img/gato-telefone.svg" usuario="meowed" usuario2="respondeai" texto="outras 101.523 pessoas" />
+    <Post imagem="assets/img/barked.svg" imagem2="assets/img/dog.svg" usuario="barked" usuario2="adorable_animals" texto="outras 99.159 pessoas" />
+  </div>
+    )
+}
